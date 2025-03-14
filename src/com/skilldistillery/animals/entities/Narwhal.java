b01d2e40723a440585929ba043da61c0d8370a14
Kirstin <kirstin.eells98@gmail.com>
2025-03-14 16:59:56 -0600

    Animal classes update

--- src/com/skilldistillery/animals/entities/Narwhal.java
+++ src/com/skilldistillery/animals/entities/Narwhal.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.animals.entities;
 
 public class Narwhal {
-
+	private String noise;
+	
+	@Override
+	public void makeNoise() {
+		
+	}
+	@Override
+	public void eat(int amount) {
+		
+	}
 }
--- src/com/skilldistillery/animals/entities/Walrus.java
+++ src/com/skilldistillery/animals/entities/Walrus.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.animals.entities;
 
 public class Walrus {
-
+	private String noise;
+	
+	@Override
+	public void makeNoise() {
+		
+	}
+	@Override
+	public void eat(int amount) {
+		
+	}
 }
