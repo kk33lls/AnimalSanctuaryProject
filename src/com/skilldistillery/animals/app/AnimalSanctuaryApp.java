519346745c9680a026ab57bc517e727a8e8371f5
Kirstin <kirstin.eells98@gmail.com>
2025-03-14 16:57:12 -0600

    Animal class update

--- src/com/skilldistillery/animals/app/AnimalSanctuaryApp.java
+++ src/com/skilldistillery/animals/app/AnimalSanctuaryApp.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class AnimalSanctuaryApp {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		AnimalSanctuaryApp app = new AnimalSanctuaryApp();
+		app.launchApp();
+		Scanner kb = new Scanner(System.in);
 
+	}
+	private void launchApp() {
+		//TODO instantiate a Sanct
+		//TODO instantiate a attendant
+		//TODO Add attendant to sanct
+		
+		
+		//TODO Create a and add animals to Sanct
+		
+		//TODO tell sanct to start attendant rounds (last thing)
 	}
 
 }
--- src/com/skilldistillery/animals/entities/Animal.java
+++ src/com/skilldistillery/animals/entities/Animal.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.animals.entities;
 
 public class Animal {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void makeNoise() {
+		
+	}
+	public void eat(int amount) {
+		
+	}
 }
