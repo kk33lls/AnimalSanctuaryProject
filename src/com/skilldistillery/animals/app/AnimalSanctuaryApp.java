7c474148bf360db316cb2995d2e607e1af4e5fc2
Kirstin <kirstin.eells98@gmail.com>
2025-03-14 23:31:10 -0600

    End of day submission

--- src/com/skilldistillery/animals/app/AnimalSanctuaryApp.java
+++ src/com/skilldistillery/animals/app/AnimalSanctuaryApp.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.animals.app;
 import java.util.Scanner;
 
+import com.skilldistillery.animals.entities.Animal;
 import com.skilldistillery.animals.entities.Attendant;
 public class AnimalSanctuaryApp {
 
@@ -11,19 +12,48 @@ public class AnimalSanctuaryApp {
 	private void launchApp() {
 		Scanner kb = new Scanner(System.in);
 		boolean keepGoing = true;
+		Animal animal = new Animal();//Is this ok?
 		Sanctuary sanctuary = new Sanctuary();
 		Attendant attendant = new Attendant();
-		
+
 		do {
-		
-		//TODO Add attendant to sanct
-		
-		//TODO Create a loop? and add animals to Sanct
-		
-		//TODO Have sanct list animals
-		
-		//TODO tell sanct to start attendant rounds (last thing)
-		}while(keepGoing);
+			printMenu();
+			String userOption = kb.next();
+			
+			if (userOption.equals("a") || userOption.equals("A")) {
+				sanctuary.listAnimals();
+			}
+			
+			if (userOption.equals("b") || userOption.equals("B")) {// is there a way to not hard code the sublclasses?
+				
+				System.out.println("Please select the animal you'd like to add: ");
+				
+				String animalAdd = kb.next();
+				
+				if (animalAdd.equals("dolphin")) {
+					Animal dolphin = animalAdd;
+					sanctuary.addAnimal(dolphin);
+				} else if (animalAdd.equals("walrus")) {
+					String walrus = animalAdd;
+					sanctuary.addAnimal(walrus);
+				} else if (animalAdd.equals("narwahl")) {
+					sanctuary.addAnimal(narwahl);// ? how
+				} else {
+					System.out.println("Invalid input, try again");
+				}
+			
+				if (userOption.equals("c") || userOption.equals("C")) {
+					attendant.makeRounds(null);// What value should I be passing?
+				}
+			
+				if (userOption.equals("d") || userOption.equals("D")) {
+					System.out.println("Thanks for stopping by!");
+					keepGoing = false;
+				}
+
+			}
+
+		} while (keepGoing);
 	}
 	private void printMenu() {
 		System.out.println("******************************************");
